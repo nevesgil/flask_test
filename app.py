a2c2ea6646c84192a05833fa476ca41ad3d70ff4
Gilmar Neves <gilmarneves@outlook.com>
2024-06-20 22:13:36 -0300

    Postgresql use

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def create_app(db_url=None):
         "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     )
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
-        "DATABASE_URL", "sqlite:///data.db"
+        "DATABASE_URL", "postgresql://admin:admin@postgres:5432/pucappdb" #"sqlite:///data.db"
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
